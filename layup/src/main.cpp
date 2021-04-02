a67e7a326d7757be461ea03eaa6cc804f44fd286
Shubham Mishra <smishra99.iitkgp@gmail.com>
2021-04-02 12:33:57 +0530

    VGG scaledown

--- layup/src/main.cpp
+++ layup/src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
 
 #if VGG
     std::cout << "VGG Model selected" << std::endl;
-    model->add("conv", { 16, 3, 1 });
+    model->add("conv", { 16, 5, 1 });
     model->add("relu");
     model->add("conv", { 64, 3, 1 });
     model->add("relu");
